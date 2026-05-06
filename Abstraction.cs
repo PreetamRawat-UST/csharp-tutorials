1375925a73dc1da30da720d536736646cdca320c
Preetam Rawat(UST <309919@ust.com>
2026-05-06 16:34:48 +0530

    Add abstraction

---
+++ Abstraction.cs
@@ -0,0 +1,30 @@
+using System;
+
+abstract class Birds
+{
+    virtual public void Fly()
+    {
+        Console.WriteLine("Bird flies!");
+    }
+
+    public abstract void MakeSound();
+}
+
+
+class Parrot : Birds
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Parrot makes a sound!");
+    }
+}
+class Abstraction
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Birds parrotObj = new Parrot();
+        parrotObj.Fly();
+        parrotObj.MakeSound();
+    }
+}
