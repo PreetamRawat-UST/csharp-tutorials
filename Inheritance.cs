5e22e493d227f6acd2f109d88e4d32e039711763
Preetam Rawat(UST <309919@ust.com>
2026-05-06 16:03:31 +0530

    First commit

---
+++ Inheritance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Vehicle
+{
+    virtual public void Honk()
+    {
+        Console.WriteLine("Beep beep!");
+    }
+}
+
+class Car : Vehicle
+{
+    public override void Honk()
+    {
+        Console.WriteLine("Car  honk!");
+    }
+}
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Vehicle carObj = new Car();
+        carObj.Honk();
+    }
+}
